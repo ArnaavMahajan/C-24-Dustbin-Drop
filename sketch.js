9a18969185f90381747961ca4d85b8de86ba46c9
ArnaavMahajan <62941728+ArnaavMahajan@users.noreply.github.com>
2020-07-15 14:37:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+const { Engine, World, Bodies, Body, Mouse, MouseConstraint, Constraint, Composite, Detector } = Matter;
+
+var square;
+
+function setup() {
+    createCanvas(400,400);
+rect(200,200,10,10);
+}
+
+function draw() {
+background = "red";
+
+}
+
+
+function keyReleased() {
+   
+}
